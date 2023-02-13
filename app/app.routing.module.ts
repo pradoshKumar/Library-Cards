600120d702780a58051ba5bc2353935c8094c817
pradoshKumar <33890313+pradoshKumar@users.noreply.github.com>
2023-02-13 10:17:39 +0530

    3 files modified, 3 added

--- app/app.routing.module.ts
+++ app/app.routing.module.ts
@@ -3,25 +3,25 @@ import { NgModule } from '@angular/core';
 import { LibraryCardV2ViewComponent } from './cards/librarycard-v2/librarycard-v2.component';
 import { LibraryCardViewComponent } from './cards/librarycard/librarycard.component';
 import { GroupsViewComponent } from './cards/groups/groups.component';
+import { LibraryCardV3ViewComponent } from './cards/librarycard-v3/librarycard-v3.component';
 
 @NgModule({
-  declarations: [ 
-    LibraryCardV2ViewComponent, LibraryCardViewComponent, GroupsViewComponent
+  declarations: [
+    LibraryCardV3ViewComponent,
+    LibraryCardV2ViewComponent,
+    LibraryCardViewComponent,
+    GroupsViewComponent,
   ],
   imports: [
     RouterModule.forRoot([
+      { path: 'librarycard-v3', component: LibraryCardV3ViewComponent },
       { path: 'librarycard-v2', component: LibraryCardV2ViewComponent },
       { path: 'librarycard', component: LibraryCardViewComponent },
       { path: 'groups', component: GroupsViewComponent },
-      { path: '**', redirectTo: 'groups' }
-    ])
+      { path: '**', redirectTo: 'groups' },
+    ]),
   ],
-  exports: [
-    RouterModule,
-  ],
+  exports: [RouterModule],
   providers: [],
-
 })
 export class AppRoutingModule {}
-
-
---
+++ app/cards/librarycard-v3/librarycard-v3.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'librarycard-v3',
+  templateUrl: './librarycard-v3.component.html',
+  styleUrls: ['./librarycard-v3.component.scss'],
+})
+export class LibraryCardV3ViewComponent {}
